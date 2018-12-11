fa9be62316e9728bc107e6b75156db74594ef8e3
ocean_global <oceanglobal@qq.com>
2018-12-11 17:04:18 +0800

    first commit

---
+++ src/main/java/com/atguigu/acfd/constant/AttrNameConstant.java
@@ -0,0 +1,16 @@
+package com.atguigu.acfd.constant;
+
+/**
+ * 属性名常量类
+ * @author Ocean
+ *
+ */
+public final class AttrNameConstant {
+    private AttrNameConstant() {}
+    
+    public static final String SESSION_LOGIC_USER = "LOGIC_USER";
+    
+    public static final String MESSAGE = "MESSAGE";
+    
+
+}
---
+++ src/main/java/com/atguigu/acfd/constant/ErrorMessageConstant.java
@@ -0,0 +1,11 @@
+package com.atguigu.acfd.constant;
+
+public class ErrorMessageConstant {
+	
+	public static final String LOGIN_FAILED = "您的用户名、密码不正确，请重新输入！";
+	public static final String ACCESS_FORBIDDEN = "抱歉！请登录后再访问这个资源！";
+    public static final String MD5_SOURCE_MISSING = "你用来MD5加密的源字符串丢了！";
+    public static final String ACCT_NOT_EXISTS = "您登录的账号不存在！";
+    public static final String PWD_NOT_MATCH = "您填写的密码不正确！";
+
+}
